c89abb6077a51389b3cf36b900ae5fd154fe84d8
Aloysius <aloysius.tan00@gmail.com>
2026-02-23 20:45:11 +0800

    Commit 2

--- bankSimul.cpp
+++ bankSimul.cpp
@@ -8,27 +8,41 @@ class BankAccount {
     double balance;
 
     public:
+    BankAccount(string accountHolderName) {
+        this->accountHolderName = accountHolderName;
+        balance = 0;
+    }
+
     int deposit(double amount) {
-        return balance + amount;
+        balance = balance + amount;
+        cout << "Deposited: " << balance << endl;
+        return balance;
     }
 
     int withdraw(double amount) {
-        if (balance != 0 && balance - amount > 0) {
-            return balance - amount;
+        if (balance >= amount) {
+            balance = balance - amount;
+            cout << "Withdrawn: " << balance << endl;
+            return balance;
         } else {
             cout << "The fund amount that you wish to withdraw exceeds your current balance." << endl;
+            return 0;
         }
     }
 
-    int displayBalance(string accountHolderName) {
+    int displayBalance() {
+        cout << "Account Holder: " << accountHolderName << endl;
+        cout << "Current Balance: " << balance << endl;
         return balance;
     }
 
 };
 
 int main() {
-    BankAccount bankAccount;
-    double balance = 0;
+    BankAccount myAccount("Aloysius");
 
-
+    myAccount.deposit(500.57);
+    myAccount.withdraw(300);
+    myAccount.deposit(780.57);
+    myAccount.displayBalance();
 }
